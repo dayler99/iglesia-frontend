54b72afa2486477fc161a8e0c443bee6f3c5d4b0
dayler <daylertaboada@mail.com>
2024-01-04 17:21:33 -0400

    m

--- src/helpers/Url.js
+++ src/helpers/Url.js
@@ -1,4 +1,4 @@
 export const urlPhpMicroservicio = 'http://107.20.34.206/api/';
 //export const urlPythonMicroservicio = 'http://192.168.0.8:5000/';
-export const urlPythonMicroservicio = 'http://64.23.148.139/';
+export const urlPythonMicroservicio = 'http://64.23.148.139:8001/';
 export const urlNodeMicroservicio = 'https://proyecto-iglesia-410014.ue.r.appspot.com/';
